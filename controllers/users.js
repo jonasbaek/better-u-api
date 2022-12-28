6dfaf340b7f1032e878824033e7d9bfbcd488809
jonasbaek <jonasbaek@hotmail.com>
2022-12-28 01:41:47 -0300

    Create mvc structure for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,17 @@
-const create = (req, res) => {
+const userService = require("../services/user");
+
+const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
   if (!name || !username || !email || !password) {
     res.status(401).send({ error: "Submit all fields for registration" });
   }
+  const user = await userService.create(req.body);
+  if (!user) {
+    return res.status(400).send({ message: "Error creating user" });
+  }
   res.status(201).send({
     user: {
+      id: user._id,
       name,
       username,
       email,
