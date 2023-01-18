69c4595950a470bb416a73b9f89099fb3435b789
jonasbaek <jonasbaek@hotmail.com>
2023-01-18 04:35:37 -0300

    Remove post and add likes on comments schema

--- models/Comments.js
+++ models/Comments.js
@@ -15,11 +15,19 @@ const CommentSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
-    required: true,
-  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
 });
 const Comment = mongoose.model("Comment", CommentSchema);
 
