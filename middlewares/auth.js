2ceff85d79614b74ef40eba3f4b7e5897ecc19c3
jonasbaek <jonasbaek@hotmail.com>
2023-02-11 09:12:53 -0300

    Refactor auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,28 +2,23 @@ import jwt from "jsonwebtoken";
 import userService from "../services/user.js";
 
 export const authMiddleware = async (req, res, next) => {
-  try {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      return res.sendStatus(401);
-    }
-    const parts = authorization.split(" ");
-    const [schema, token] = parts;
-    if (schema !== "Bearer") {
-      return res.sendStatus(401);
-    }
-    jwt.verify(token, process.env.SECRET_JWT, async (error, decoded) => {
-      if (error) {
-        return res.status(401).send({ message: "Invalid Token!" });
-      }
-      const user = await userService.findByIdService(decoded.id);
-      if (!user || !user.id) {
-        return res.status(401).send({ message: "Invalid token!" });
-      }
-      req.currentUser = user;
-      return next();
-    });
-  } catch (error) {
-    res.status(500).send(error.message);
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send({ message: "Missing token" });
   }
+
+  const [, token] = authorization.split(" ");
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_JWT);
+    const user = await userService.findByIdService(decoded.id);
+    if (!user) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+    req.currentUser = user;
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+
+  next();
 };
